6cfae2d6e8b1e6b0f6c50907eaf3bb42f4ece28d
Claire Tregunna <c.tregunna1980@gmail.com>
2021-03-02 14:24:22 +0000

    day 14 post

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const showWeekOne = document.querySelector('.open_week_1');
 showWeekOne.addEventListener('click', () => {
     weekOne.classList.toggle('hidden');
 });
+
+//Reveal type coercion answer
+const reveal = document.querySelector(".reveal");
+const answer = document.querySelector(".answer");
+reveal.addEventListener('click', () =>{
+    answer.classList.toggle("show");
+}
+
+)

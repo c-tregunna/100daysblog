7a29b4640111e95c61f9284ee61f50fcc5cddad1
Claire Tregunna <c.tregunna1980@gmail.com>
2021-03-07 14:36:48 +0000

    day 15 post and added 2nd week button. refactor button js code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-const weekOne = document.querySelector('.weekly_wrapper .week_1');
-const showWeekOne = document.querySelector('.open_week_1');
+// const weeks = document.querySelectorAll('.weekly_wrapper>.week');
+// console.log(weeks);
+// const weekTwo = document.querySelector('.weekly_wrapper .week_2');
+// const showWeekOne = document.querySelector('.open_week_1');
+// const showWeekTwo = document.querySelector('.open_week_2');
 
-showWeekOne.addEventListener('click', () => {
-    weekOne.classList.toggle('hidden');
-});
+// showWeekOne.addEventListener('click', () => {
+//     weekOne.classList.toggle('hidden');
+// });
+
+// showWeekTwo.addEventListener('click', () => {
+//     weekTwo.classList.toggle('hidden');
+// });
+
+
+const openWeek = document.querySelectorAll('.open');
+console.log(openWeek);
+
+openWeek.forEach(open => {
+    open.addEventListener('click', () => {
+        open.nextElementSibling.firstElementChild.classList.toggle('hidden');
+        })
+    });
+
+
 
 //Reveal type coercion answer
 const reveal = document.querySelector(".reveal");

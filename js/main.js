e13eaea45696350fa286146f260301867bbab3f9
Claire Tregunna <c.tregunna1980@gmail.com>
2021-03-08 14:46:12 +0000

    day 16 post

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// Week buttons to open each week and will open the closest weekly wrapper
+const openWeek = document.querySelectorAll('.open');
+console.log(openWeek);
+
+openWeek.forEach(open => {
+    open.addEventListener('click', () => {
+        open.nextElementSibling.firstElementChild.classList.toggle('hidden'); //adds class to the weekly wrapper(nextelement) week(firstelementchild) div
+        console.log(open);
+    })
+});
+
+
+
+//Reveal type coercion answer
+const reveal = document.querySelector(".reveal");
+const answer = document.querySelector(".answer");
+reveal.addEventListener('click', () => {
+    answer.classList.toggle("show");
+});
+
 // const weeks = document.querySelectorAll('.weekly_wrapper>.week');
 // console.log(weeks);
 // const weekTwo = document.querySelector('.weekly_wrapper .week_2');
@@ -11,25 +31,3 @@
 // showWeekTwo.addEventListener('click', () => {
 //     weekTwo.classList.toggle('hidden');
 // });
-
-
-const openWeek = document.querySelectorAll('.open');
-console.log(openWeek);
-
-openWeek.forEach(open => {
-    open.addEventListener('click', () => {
-        open.nextElementSibling.firstElementChild.classList.toggle('hidden');
-        console.log(open);
-        })
-    });
-
-
-
-//Reveal type coercion answer
-const reveal = document.querySelector(".reveal");
-const answer = document.querySelector(".answer");
-reveal.addEventListener('click', () =>{
-    answer.classList.toggle("show");
-}
-
-)
